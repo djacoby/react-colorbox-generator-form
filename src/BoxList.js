d52e53f585a14165dc23ba69ab4ad76676c3d690
David Jacoby <jacoby934@gmail.com>
2019-07-19 22:00:12 -0400

    Second Commit

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import NewBoxForm from "./NewBoxForm";
 import Box from "./Box";
-import uuid from "uuid/v4";
 import "./BoxList.css";
 
 export default class BoxList extends Component {
