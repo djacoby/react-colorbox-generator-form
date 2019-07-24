ab503f228997285213a9047efaa47968ae800be4
David Jacoby <jacoby934@gmail.com>
2019-07-23 21:34:48 -0400

    Third commit

--- src/Box.js
+++ src/Box.js
@@ -6,34 +6,23 @@ export default class Box extends Component {
     this.state = {
       visible: true
     };
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    this.setState(state => ({
-      visible: false
-    }));
   }
 
   render() {
-    if (this.state.visible === true) {
-      return (
-        <div className="Box">
-          <div
-            style={{
-              key: `${this.props.id}`,
-              backgroundColor: `${this.props.color}`,
-              height: `${this.props.height}rem`,
-              width: `${this.props.width}rem`
-            }}
-          />
-          <button className="btn btn-danger" onClick={this.props.removeBox}>
-            Remove Box
-          </button>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    return (
+      <div className="Box">
+        <div
+          style={{
+            key: `${this.props.id}`,
+            backgroundColor: `${this.props.color}`,
+            height: `${this.props.height}rem`,
+            width: `${this.props.width}rem`
+          }}
+        />
+        <button className="btn btn-danger" onClick={this.props.removeBox}>
+          Remove Box
+        </button>
+      </div>
+    );
   }
 }
